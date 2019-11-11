e1dd203d6a85778898e912e359f143f9e396af69
Hwanghuntae <ghkdgnsxo2484@naver.com>
2019-11-11 13:52:30 +0900

    Key 리스너 구현

--- MyHelloFrame.java
+++ MyHelloFrame.java
@@ -7,13 +7,25 @@ import javax.swing.*;
  * @author (작성자 이름) 
  * @version (버전번호나 날짜)
  */
-public class MyHelloFrame 
+public class MyHelloFrame extends JFrame
 {
+
+    private JLabel jl = new JLabel("HELLO");
     public MyHelloFrame(){
-        
-        //jp.addMouseListener(new MyHelloFrameListener(jp,jl));
-        //jp.add(jl);
-        
-        //add(jp);
+        setTitle("실습#3(황훈태 2018315009, 정대기 2018315026");
+
+        JPanel jp = new JPanel();
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        jp.addMouseListener(new MyHelloFrameListener(jp,jl));
+        jp.addKeyListener(new MyHelloFrameListener(jp,jl));
+        jp.add(jl);
+
+        add(jp);
+
+        setSize(300,300);
+        setVisible(true);
+
+        jp.setFocusable(true);
+
     }
 }
--- MyHelloFrameListener.java
+++ MyHelloFrameListener.java
@@ -7,18 +7,57 @@ import javax.swing.*;
  * @author (작성자 이름) 
  * @version (버전번호나 날짜)
  */
-public class MyHelloFrameListener extends JFrame
+public class MyHelloFrameListener extends KeyAdapter implements MouseListener
 
 {
-    public MyHelloFrameListener() {
-        JPanel jp = new JPanel();
+    private final int FLYING_UNIT = 10;
+    JPanel jp;    
+    JLabel jl;
+    public MyHelloFrameListener(JPanel jp, JLabel jl)
+    {
+        this.jp = jp;
+        this.jl = jl;
+    }
 
-        
-        setTitle("실습#3(황훈태 2018315009, 정대기 2018315026");
-        setSize(300,300);
+    public void mouseClicked(MouseEvent e){
 
-        setVisible(true);
+    }
 
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
+    public void mouseEntered(MouseEvent e){
+
+    }
+
+    public void mouseExited(MouseEvent e){
+
+    }
+
+    public void mousePressed(MouseEvent e){
+        int x = e.getX();
+        int y = e.getY();
+        jl.setLocation(x, y);
+    }
+
+    public void mouseReleased(MouseEvent e){
+
+    }
+
+    public void keyPressed(KeyEvent e){
+        int keyCode = e.getKeyCode();  // 입력된 키의 키코드를 알아낸다.
+        // 키 코드 값(keyCode)에 상,하,좌,우 키를 판별하고 la의 위치를 이동시킨다.
+        switch(keyCode) {
+            case KeyEvent.VK_UP:
+                jl.setLocation(jl.getX(), jl.getY()-FLYING_UNIT);
+                
+            case KeyEvent.VK_DOWN:
+                jl.setLocation(jl.getX(), jl.getY()+FLYING_UNIT); 
+              
+            case KeyEvent.VK_LEFT:
+                jl.setLocation(jl.getX()-FLYING_UNIT, jl.getY());
+            
+            case KeyEvent.VK_RIGHT:
+                jl.setLocation(jl.getX()+FLYING_UNIT, jl.getY()); 
+           
+        }
     }
 }
+
